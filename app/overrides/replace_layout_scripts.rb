6abb3c50c41f13fb58f716d370784d1c2b720184
Brian Quinn <briandquinn@gmail.com>
2013-01-15 10:45:35 +0000

    Remove old theme features

--- app/overrides/replace_layout_scripts.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(:name => 'replace_layout_scripts',
-                     :virtual_path => 'spree/layouts/spree_application',
-                     :sequence => {:after => '_spraycan_ui'},
-                     :replace => %q{code[erb-loud]:contains('javascript_include_tag "/spraycan/compiled')},
-                     :closing_selector => %q{code[erb-loud]:contains('stylesheet_link_tag "/spraycan/compiled')}) do
-  %q{<%= javascript_include_tag "/spraycan/#{ENV['RAILS_CACHE_ID']}/compiled/#{Spraycan::Config[:javascript_digest]}" %>
-<%= stylesheet_link_tag "/spraycan/#{ENV['RAILS_CACHE_ID']}/compiled/#{Spraycan::Config[:stylesheet_digest]}", :id => 'compiled_stylesheets' %>}
-end
