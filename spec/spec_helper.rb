79bbd3aacda895f6ebc2499cae3905a491cc7b75
Tobias Bohwalli <hi@futhr.io>
2015-01-22 19:33:29 +0100

    Cleanup spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+begin
+  require File.expand_path('../dummy/config/environment', __FILE__)
+rescue LoadError
+  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
+  exit
+end
 
 # switch apartment adapter directory to dummy app
 Thread.current[:apartment_adapter].instance_variable_set("@default_dir", File.expand_path("../dummy/db",  __FILE__))
@@ -9,27 +13,11 @@ Thread.current[:apartment_adapter].instance_variable_set("@default_dir", File.ex
 require 'spree_shared'
 require 'rspec/rails'
 require 'apartment'
-require 'pry'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
 end
+
+Dir[File.join(File.dirname(__FILE__), '/support/**/*.rb')].each { |file| require file }
